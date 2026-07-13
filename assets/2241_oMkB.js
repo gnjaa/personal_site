dbe839145221b8abcb4fe202c4e2eba29724987f
gisl <gnjaastad@gmail.com>
2026-07-12 20:17:36 -0500

    wordpress migration
    in progress!

---
+++ assets/2241_oMkB.js
@@ -0,0 +1,13 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+document.addEventListener('DOMContentLoaded', function () {
+  const link = document.getElementById('eLink');
+  if (!link) return;
+
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.location.href =
+      'mailto:' + 'gnjaastad' + '@' + 'gmail.com';
+  });
+});</script>
+<!-- end Simple Custom CSS and JS -->
